6a63d47e060933738631c7ea33e4113d0c52ccb2
pepeferrer30 <119110456+pepeferrer30@users.noreply.github.com>
2022-11-26 12:48:25 +0100

    Update ConsultaBrutoController.java

--- ConsultaBrutoController.java
+++ ConsultaBrutoController.java
@@ -1,8 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
-package simulacro.pkg4;
+
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -15,11 +11,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-/**
- * FXML Controller class
- *
- * @author Joan_2k2
- */
+
 public class ConsultaBrutoController implements Initializable {
 
     @FXML
